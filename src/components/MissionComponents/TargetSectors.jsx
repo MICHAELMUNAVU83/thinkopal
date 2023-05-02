fd0f9c72b5c227e17ac2b253b90d3e9ee718d591
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-05-02 14:50:06 +0300

    Added target sectors

--- src/components/MissionComponents/TargetSectors.jsx
+++ src/components/MissionComponents/TargetSectors.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import targetpic from "../images/targetsectorspic.png";
-import missio from "../images/missio.svg";
-
 const TargetSectors = () => {
   return (
-    <div className="py-2 bg-[#FFD230] pt-20">
-      <div className="flex justify-around mx-auto md:w-[80%]">
-        <div className="text-2xl gap-1 text-start flex justify-center flex-col">
+    <div className="py-2 bg-[#FFD230] ">
+      <div className=" flex flex-col-reverse mt-10 md:flex-row justify-around mx-auto w-[80%]">
+        <div className="  text-2xl gap-1  text-start flex justify-center flex-col">
           <ul className="list-disc quicksand-500">
             <li className="my-1">HealthCare</li>
             <li className="my-1">Education</li>
@@ -19,10 +17,13 @@ const TargetSectors = () => {
             <li className="my-1">Blue Economy</li>
           </ul>
         </div>
-
-        <div className="hidden md:flex ">
-          <img src={targetpic} alt="whatpic" className="h-[500px] w-[500px]" />
-          <div className="absolute top-2/3 left-[29%] alstoria text-center  text-7xl font-bold flex flex-col ">
+        <div className="relative">
+          <img
+            src={targetpic}
+            alt="whatpic"
+            className="md:h-[500px] md:w-[500px]"
+          />
+          <div className="absolute top-1/4 left-[20%] md:top-2/3 md:left-[29%] alstoria text-center  text-7xl font-bold flex flex-col ">
             <p className="text-5xl">Target</p>
             <p>Sectors</p>
           </div>

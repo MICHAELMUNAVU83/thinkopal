6d94981a59d6bfb21c57197218b1e3716161c19b
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-02 18:45:22 +0300

    Styed mbinu showcase

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ProductsHero from "../components/ProductsComponents/ProductsHero";
-import SustainableGoals from "../components/SustainableGoals";
-import MbinuShowCase from "../components/ProductsComponents/MbinuShowcase";
 import Inquiry from "../components/Inquiry";
 import Coaching from "../components/ProductsComponents/Coaching";
 import CapacityBuilding from "../components/ProductsComponents/CapacityBuilding";
 import Mbinu from "../components/ProductsComponents/Mbinu";
 import FreeDownloads from "../components/ProductsComponents/FreeDownloads";
+import MbinuShowCase from "../components/ProductsComponents/MbinuShowcase";
 const Products = () => {
   return (
     <div>

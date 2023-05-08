9a291009bc7638b3435176da3315ff190da98b2c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-08 05:54:42 +0300

    Added bg widths

--- src/components/ContactComponents/Location.jsx
+++ src/components/ContactComponents/Location.jsx
@@ -5,7 +5,7 @@ const Location = () => {
   return (
     <div className="md:bg-white bg-[#FFD230]">
       <div className="w-[80%] mx-auto md:my-8 flex items-center justify-between">
-        <p className="md:w-[40%] w-[100%] text-center py-8 md:py-0 md:text-3xl  text-xl quicksand-500 ">
+        <p className="md:w-[40%] w-[100%] md:text-start  text-center py-8 md:py-0 md:text-3xl  text-xl quicksand-500 ">
           Need to get in touch with us at Think Opal? We'd love to hear from
           you! Our friendly team is ready to assist with any queries or feedback
           you may have. Drop us a message and we'll get back to you as soon as
